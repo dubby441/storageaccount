421f157d26492929fd9bfa50dec3b5c60b5183e4
dubby441 <dubby441@gmail.com>
2023-04-14 14:12:17 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.base_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+
+}
---
+++ variable.tf
@@ -0,0 +1,14 @@
+variable "base_name" {
+    type = string
+    description = "The base name for the storage account"
+}
+
+variable "resource_group_name" {
+    type = string
+    description = "name for resource group"
+}
+
+variable "location" {
+    type = string
+    description = "location for deployment"
+}
